7e39ba961a32fea01ea7db15021c457293345695
Kross55 <iyakushenko84@gmail.com>
2023-05-11 17:01:12 +0300

    weatherApp_some refactor1

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,43 +1,45 @@
 import React from "react";
 import './Weather.css';
 const Forecast = (props) => {
-
+  const { name, sys, main, wind, weather } = props.data;
+  const status = props.status;
+  const icon  = props.icon;
   return (
     <div>
-      {props.status === 'loading' && (
+      {status === 'loading' && (
         <div>
           Loading ...
         </div>
       )}
-      {props.status === 'success' && (
+      {status === 'success' && (
         <div className="city">
           <h2 className="city-name">
-            <span>{props.data.name}</span>
-            <sup>{props.data.sys.country}</sup>
+            <span>{name}</span>
+            <sup>{sys.country}</sup>
           </h2>
           <div className="city-temp">
-            {Math.round(props.data.main.temp)}<sup>°C</sup>
+            {Math.round(main.temp)}<sup>°C</sup>
           </div>
           <div className="city-pressure">
-            {props.data.main.pressure} hPa
+            {main.pressure} hPa
           </div>
           <div className="city-wind">
-            Humidity: {props.data.main.humidity} %
+            Humidity: {main.humidity} %
           </div>
           <div className="city-wind">
-            Wind: {props.data.wind.deg}<sup>°</sup> ${Math.round(props.data.wind.speed)} m/s,
+            Wind: {wind.deg}<sup>°</sup> ${Math.round(wind.speed)} m/s,
           </div>
           <div className="city-wind">
-            Gust: ${Math.round(props.data.wind.gust)} m/s
+            Gust: ${Math.round(wind.gust)} m/s
           </div>
           <figure>
-            <img className="city-icon" src={props.icon} alt={props.data.weather[0]["description"]}></img>
-            <figcaption>{props.data.weather[0]["description"]}</figcaption>
+            <img className="city-icon" src={icon} alt={weather[0]["description"]}></img>
+            <figcaption>{weather[0]["description"]}</figcaption>
           </figure>
         </div>
 
       )}
-      {props.status === 'failure' && (
+      {status === 'failure' && (
         <div className="msg pCity">
           Please search for a valid city 😩
         </div>
