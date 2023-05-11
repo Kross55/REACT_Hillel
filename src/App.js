d5bb85aa7f44760b1ae7d8268a7922b7d98476a2
Kross55 <iyakushenko84@gmail.com>
2023-05-11 13:51:58 +0300

    weatherApp_works_forCheck

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './App.css';
-import WeatherSection from "./Components/WeatherSection";
 import FormSection from "./Components/FormSection";
 import Footer from "./Components/Footer";
 
@@ -10,7 +9,6 @@ function App() {
   return (
     <div>   
       <FormSection />
-      <WeatherSection />
       <Footer />    
     </div>
   );
--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,99 +1,50 @@
 import React from "react";
 import './Weather.css';
+const Forecast = (props) => {
 
-const API_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather";
-const API_ICON = "http://openweathermap.org/img/w/";
-
-function urlHandler() {
-  //const input = document.querySelector(".inputCity");
-  //let textInput = React.createRef();
-  //input.ref = textInput;
-
-
-  const params = {
-    q: "Mexico",//event.target.value?
-    units: "metric",
-    APPID: "5d066958a60d315387d9492393935c19" 
-  };
-  
-  const query = Object
-          .keys(params)
-          .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-          .join('&');
-      
-  const url = `${API_ENDPOINT}?${query}`;
-  return url
-}
-
-class Forecast extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      data: null,
-      icon: null,
-      status: 'loading'
-    };
-  }
-
-  componentDidMount() {
-    fetch(urlHandler())
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ 
-          status: 'success', 
-          data: data,
-          icon: `${API_ICON}${data.weather[0]["icon"]}.png` })
-      })
-      .catch(() => {
-        this.setState({ status: 'failure' })
-      })
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.status === 'loading' && (
-          <div>
-            Loading ...
+  return (
+    <div>
+      {props.status === 'loading' && (
+        <div>
+          Loading ...
+        </div>
+      )}
+      {props.status === 'success' && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{props.data.name}</span>
+            <sup>{props.data.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(props.data.main.temp)}<sup>°C</sup>
           </div>
-        )}
-        {this.state.status === 'success' && (
-          <div className="city">
-            <h2 className="city-name">
-              <span>{this.state.data.name}</span>
-              <sup>{this.state.data.sys.country}</sup>
-            </h2>
-            <div className="city-temp">
-              {Math.round(this.state.data.main.temp)}<sup>°C</sup>
-            </div>
-            <div className="city-pressure">
-              {this.state.data.main.pressure} hPa
-            </div>              
-            <div className="city-wind">
-              Humidity: {this.state.data.main.humidity} %
-            </div>
-            <div className="city-wind">
-              Wind: {this.state.data.wind.deg}<sup>°</sup> ${Math.round(this.state.data.wind.speed)} m/s,
-            </div>
-            <div className="city-wind">
-              Gust: ${Math.round(this.state.data.wind.gust)} m/s
-            </div>
-            <figure>
-              <img className="city-icon" src={this.state.icon} alt={this.state.data.weather[0]["description"]}></img>
-              <figcaption>{this.state.data.weather[0]["description"]}</figcaption>
-            </figure> 
+          <div className="city-pressure">
+            {props.data.main.pressure} hPa
           </div>
-          
-        )}
-        {this.state.status === 'failure' && (
-          <div className="msg">
-            Please search for a valid city 😩
+          <div className="city-wind">
+            Humidity: {props.data.main.humidity} %
           </div>
-        )}
-      </div>
-    )
-  }
+          <div className="city-wind">
+            Wind: {props.data.wind.deg}<sup>°</sup> ${Math.round(props.data.wind.speed)} m/s,
+          </div>
+          <div className="city-wind">
+            Gust: ${Math.round(props.data.wind.gust)} m/s
+          </div>
+          <figure>
+            <img className="city-icon" src={props.icon} alt={props.data.weather[0]["description"]}></img>
+            <figcaption>{props.data.weather[0]["description"]}</figcaption>
+          </figure>
+        </div>
+
+      )}
+      {props.status === 'failure' && (
+        <div className="msg pCity">
+          Please search for a valid city 😩
+        </div>
+      )}
+    </div>
+  )
+
 }
 
 export default Forecast;
