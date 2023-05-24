fe276eb1536627acb2c6d91ef56f61c5e3ce93dc
Kross55 <iyakushenko84@gmail.com>
2023-05-24 08:48:19 +0300

    form_addSomeStyle

--- src/ComponentsFormik/FormComponent.js
+++ src/ComponentsFormik/FormComponent.js
@@ -13,7 +13,9 @@ function FormComponent() {
     phoneNumber: Yup.string()
       .matches(/^\d{12}$/, "Phone number must be 12 digits")
       .required("Phone is required"),
-    password: Yup.string().required("Password is required"),
+    password: Yup.string()
+      .min(5, "must be at least 5 characters long")
+      .required("Password is required"),
   });
 
   const initialValues = {
@@ -23,55 +25,81 @@ function FormComponent() {
     password: "",
   };
   return (
-    <div>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <div>
-              <Field type="text" name="firstName" />
-              <ErrorMessage
-                className={clsx(style.errorText)}
-                name="firstName"
-                component="div"
-              />
-            </div>
-            <div>
-              <Field type="email" name="email" />
-              <ErrorMessage
-                className={clsx(style.errorText)}
-                name="email"
-                component="div"
-              />
-            </div>
-            <div>
-              <Field type="phone" name="phoneNumber" />
-              <ErrorMessage
-                className={clsx(style.errorText)}
+    <div className={clsx(style.container)}>
+      <i style={{"--clr": "#00ff0a"}}></i>
+      <i style={{"--clr": "#ff0057"}}></i>
+      <i style={{"--clr": "#fffd44"}}></i>
+      <div className={clsx(style.login)}>
+        <h2 style={{"margin": "1px", "padding": "5px"}}>Login</h2>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <div className={clsx(style.input__box)}>
+                <Field 
+                type="text" 
+                name="firstName" 
+                placeholder="firstName" 
+                />
+                <ErrorMessage
+                  className={clsx(style.errorText)}
+                  name="firstName"
+                  component="div"
+                />
+              </div>
+              <div className={clsx(style.input__box)}>
+                <Field 
+                type="email" 
+                name="email" 
+                placeholder="abc@mail"
+                />
+                <ErrorMessage
+                  className={clsx(style.errorText)}
+                  name="email"
+                  component="div"
+                />
+              </div>
+              <div className={clsx(style.input__box)}>
+                <Field 
+                type="phone" 
                 name="phoneNumber"
-                component="div"
-              />
-            </div>
-            <div>
-              <Field type="password" name="password" />
-              <ErrorMessage name="password" component="div" />
-            </div>
-            <div>
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+                placeholder="123123123123" 
+                />
+                <ErrorMessage
+                  className={clsx(style.errorText)}
+                  name="phoneNumber"
+                  component="div"
+                />
+              </div>
+              <div className={clsx(style.input__box)}>
+                <Field 
+                type="password" 
+                name="password"
+                placeholder="Password" 
+                />
+                <ErrorMessage
+                  className={clsx(style.errorText)}
+                  name="password"
+                  component="div"
+                />
+              </div>
+              <div className={clsx(style.input__box)}>
+                <button type="submit" disabled={isSubmitting}>
+                  Sing in
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
